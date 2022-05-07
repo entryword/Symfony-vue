211578603f94e34ffbfb84c24a8d42eedb0e68e0
entryword <entryword@gmail.com>
2022-05-07 19:34:32 +0200

    install

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
+import BootstrapVue3 from 'bootstrap-vue-3'
 // import App from './components/Test';
 import App from './App.vue';
 import router from './router'
 import '../css/app.css';
 
-const app = createApp(App).use(router);
+const app = createApp(App)
+app.use(router);
+app.use(BootstrapVue3)
 app.mount('#app');
